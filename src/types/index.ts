0e7a7965615f95d6aabd26f5ad872654f17ededa
Julia Kudishina <iuliakudishina@yandex.ru>
2024-06-20 23:29:04 +0300

    gg

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,45 +1,96 @@
-// Тип описывающий категории товаров
-export type CategoryType =
- | 'другое'
- | 'софт-скил'
- | 'дополнительное'
- | 'кнопка'
- | 'хард-скил';
-
-// Интерфейс, описывающий карточку товара в магазине (карточки товаров)
+// Интерфейс данных для товара
 export interface IProduct {
 	id: string; // уникальный id
-	category: CategoryType; // категория товара
+	category: string; // категория товара
 	title: string; // название  товара
 	image: string; // ссылка на изображение товара
 	price: number | null; // цена товара, может быть null
-	description: string; // описание товара
+	description?: string; // описание товара
 }
 
-// Тип описывающий методы оплаты
-export type TOrderPayment = 'cash' | 'card';
-
-// Для описания заказа используется интерфейс IOrder: (корзина->заказ)
+// Интерфес данных для заказа
 export interface IOrder {
-	items: IProduct[]; // список купленных товаров
-	total: number; // стоимость заказа
-	payment: string; // Способ оплаты
+	payment: TPayment; // Способ оплаты
 	address: string; // Адрес доставки
 	email: string; // Электронная почта
 	phone: string; // Телефон
 }
 
-// Интерфейс описывает данные корзины покупок
-export interface IBasket { 
-  items: string[]; // Список товаров в корзине
-  total: number; // Итоговая стоимость корзины
+//Данные для корзины с товарами
+export interface IBasket {
+	items: TProductInBasket[]; 
+	total: number; 
 }
 
-// для валидации полей ввода.
-export type OrderForm = Omit<IOrder, 'total' | 'items'>;
-
-// идентификатор созданного заказа и количество списанной валюты.
+// Данные успешного ответа от сервера при отправке заказа
 export interface IOrderResult {
 	id: string;
 	total: number;
 }
+
+// Данные для каталога товара
+export interface IProductList {
+	items: IProduct[];
+	preview: string | null;
+}
+
+
+// Интерфейс модели данных товаров
+export interface IProductData {
+	products: IProduct[]; //массив обьектов карточек
+	preview: string | null; // id карточки, выбранной для просмотра в модальном окне или добавленной в корзину
+	getProduct(productId: string): IProduct; //получение выбранной карточки товара по id для просмотра
+	setProducts(products: IProduct[]): void; // принимает массив и обновляет свойство products
+}
+
+// Интерфейс данных для формы заказа
+export interface IOrderData {
+	payment: TPayment; // Способ оплаты
+	address: string; // Адрес доставки
+	email: string; // Электронная почта
+	phone: string; // Телефон
+  }
+
+// Интерфейс модели данных корзины
+export interface IBasketData {
+	items: IProductInBasket[]; // список товаров в корзине
+	total: number; // общая стоимость корзины
+
+
+
+	clearBasket(): void;
+}
+
+
+
+
+// Тип описывающий категории товаров
+export type TCategoryType =
+	| 'софт-скил'
+	| 'другое'
+	| 'дополнительное'
+	| 'кнопка'
+	| 'хард-скил';
+
+// Тип для товара в корзине
+export type TProductInBasket = Pick<IProduct, 'id' | 'title' | 'price'> & {
+	index: number;
+};
+
+//  Тип способа оплаты
+export type TPayment = 'cash' | 'card';
+
+// тип модального окна выбора способа оплаты и ввода адреса
+export type TDeliveryInfo = Pick<IOrder, 'payment' | 'address'>;
+
+//  тип модального окна ввод почты и телефона (контакты)
+export type TContactsInfo = Pick<IOrder, 'email' | 'phone'>;
+
+// Тип для общей суммы заказа
+export type TTotalPrice = Pick<IBasket, 'total'>;
+
+
+
+
+
+
